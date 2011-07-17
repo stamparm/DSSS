62e3c6882dd9c6080940778504ec61e9c945e472
Miroslav Stampar <miroslav.stampar@gmail.com>
2011-07-18 00:21:06 +0200

    second commit

--- dsss.py
+++ dsss.py
@@ -8,26 +8,27 @@ AUTHOR  = "Miroslav Stampar (http://unconciousmind.blogspot.com | @stamparm)"
 LICENSE = "GPLv2 (www.gnu.org/licenses/gpl-2.0.html)"
 NOTE    = "This is a fully working PoC proving that commercial (SQLi) scanners can be beaten under 100 lines of code (blind, error, depth 1 crawler)"
 
-INVALID_SQL_CHAR_POOL = ['(',')','\'','"']
-SUFFIXES = ["", "-- ", "#"]
-PREFIXES = [" ", ") ", "' ", "') "]
-BOOLEANS = ["AND %d=%d", "OR NOT (%d=%d)"]
-MINVAL, MAXVAL = 100, 255
-TEXT, HTTPCODE, TITLE, HTML = range(4)
+INVALID_SQL_CHAR_POOL = ['(',')','\'','"']      # characters used for SQL poisoning of parameter values
+PREFIXES = [" ", ") ", "' ", "') "]             # prefix values used for building testing blind payloads
+SUFFIXES = ["", "-- ", "#"]                     # suffix values used for building testing blind payloads
+BOOLEAN_TESTS = ["AND %d=%d", "OR NOT (%d=%d)"] # boolean tests used for building testing blind payloads
+TEXT, HTTPCODE, TITLE, HTML = range(4)          # enumerator-like values used for marking content type
+MINVAL, MAXVAL = 100, 255                       # minimum and maximum range values used in blind tests
+FUZZY_THRESHOLD = 0.95                          # value in range (0,1) for distinguishing True from False responses
 
 DBMS_ERRORS = {
-    "MySQL" : (r"SQL syntax.*MySQL", r"Warning.*mysql_.*", r"valid MySQL result", r"MySqlClient\."),\
-    "PostgreSQL" : (r"PostgreSQL.*ERROr", r"Warning.*\Wpg_.*", r"valid PostgreSQL result", r"Npgsql\."),\
-    "Microsoft SQL Server" : (r"Driver.* SQL[\-\_\ ]*Server", r"OLE DB.* SQL Server", r"(\W|\A)SQL Server.*Driver", r"Warning.*mssql_.*", r"(\W|\A)SQL Server.*[0-9a-fA-F]{8}", r"Exception Details:.*\WSystem\.Data\.SqlClient\.", r"Exception Details:.*\WRoadhouse\.Cms\."),\
-    "Microsoft Access" : (r"Microsoft Access Driver", r"JET Database Engine", r"Access Database Engine"),\
-    "Oracle" : (r"ORA-[0-9][0-9][0-9][0-9]", r"Oracle error", r"Oracle.*Driver", r"Warning.*\Woci_.*", r"Warning.*\Wora_.*"),\
-    "IBM DB2" : (r"CLI Driver.*DB2", r"DB2 SQL error", r"db2_connect\(", r"db2_exec\(", r"db2_execute\(", r"db2_fetch_"),\
-    "Informix" : (r"Exception.*Informix"),\
-    "Firebird" : (r"Dynamic SQL Error", r"Warning.*ibase_.*"),\
-    "SQLite" : (r"SQLite/JDBCDriver", r"SQLite.Exception", r"System.Data.SQLite.SQLiteException", r"Warning.*sqlite_.*", r"Warning.*SQLite3::"),\
-    "SAP MaxDB" : (r"SQL error.*POS([0-9]+).*", r"Warning.*maxdb.*"),\
-    "Sybase" : (r"Warning.*sybase.*", r"Sybase message", r"Sybase.*Server message.*"),\
-    "Ingres" : (r"Warning.*ingres_", r"Ingres SQLSTATE", r"Ingres\W.*Driver")
+    "MySQL" : [r"SQL syntax.*MySQL", r"Warning.*mysql_.*", r"valid MySQL result", r"MySqlClient\."],\
+    "PostgreSQL" : [r"PostgreSQL.*ERROr", r"Warning.*\Wpg_.*", r"valid PostgreSQL result", r"Npgsql\."],\
+    "Microsoft SQL Server" : [r"Driver.* SQL[\-\_\ ]*Server", r"OLE DB.* SQL Server", r"(\W|\A)SQL Server.*Driver", r"Warning.*mssql_.*", r"(\W|\A)SQL Server.*[0-9a-fA-F]{8}", r"Exception Details:.*\WSystem\.Data\.SqlClient\.", r"Exception Details:.*\WRoadhouse\.Cms\."],\
+    "Microsoft Access" : [r"Microsoft Access Driver", r"JET Database Engine", r"Access Database Engine"],\
+    "Oracle" : [r"ORA-[0-9][0-9][0-9][0-9]", r"Oracle error", r"Oracle.*Driver", r"Warning.*\Woci_.*", r"Warning.*\Wora_.*"],\
+    "IBM DB2" : [r"CLI Driver.*DB2", r"DB2 SQL error", r"db2_connect\(", r"db2_exec\(", r"db2_execute\(", r"db2_fetch_"],\
+    "Informix" : [r"Exception.*Informix"],\
+    "Firebird" : [r"Dynamic SQL Error", r"Warning.*ibase_.*"],\
+    "SQLite" : [r"SQLite/JDBCDriver", r"SQLite.Exception", r"System.Data.SQLite.SQLiteException", r"Warning.*sqlite_.*", r"Warning.*SQLite3::"],\
+    "SAP MaxDB" : [r"SQL error.*POS([0-9]+).*", r"Warning.*maxdb.*"],\
+    "Sybase" : [r"Warning.*sybase.*", r"Sybase message", r"Sybase.*Server message.*"],\
+    "Ingres" : [r"Warning.*ingres_", r"Ingres SQLSTATE", r"Ingres\W.*Driver"]
 }
 
 def retrieveContent(url):
@@ -70,7 +71,7 @@ def scanPage(url):
                 original = retrieveContent(link)
                 a, b = random.randint(MINVAL, MAXVAL), random.randint(MINVAL, MAXVAL)
                 for prefix in PREFIXES:
-                    for boolean in BOOLEANS:
+                    for boolean in BOOLEAN_TESTS:
                         for suffix in SUFFIXES:
                             if not vulnerable:
                                 template = "%s%s%s" % (prefix, boolean, suffix)
@@ -80,7 +81,7 @@ def scanPage(url):
                                     vulnerable = True
                                 else:
                                     ratios = map(lambda x: difflib.SequenceMatcher(None, original[TEXT], x).quick_ratio(), [contents[True][TEXT], contents[False][TEXT]])
-                                    vulnerable = ratios[0] > 0.95 and ratios[1] < 0.95
+                                    vulnerable = ratios[0] > FUZZY_THRESHOLD and ratios[1] < FUZZY_THRESHOLD
                                 if vulnerable:
                                     print " (i) parameter '%s' appears to be SQLi vulnerable! (\"%s\")" % (match.group('parameter'), payloads[True])
                                     retVal = True
