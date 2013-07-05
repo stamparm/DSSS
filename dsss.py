3823ca5cfbc2812d8d91db3a6b7fd78e1a50f52f
Miroslav Stampar <miroslav.stampar@gmail.com>
2013-07-05 10:34:58 +0200

    Adding suffix %16 (MsAccess specific)

--- dsss.py
+++ dsss.py
@@ -3,12 +3,12 @@
 import difflib, httplib, itertools, optparse, random, re, urllib2, urlparse
 
 NAME    = "Damn Small SQLi Scanner (DSSS) < 100 LOC (Lines of Code)"
-VERSION = "0.2b"
+VERSION = "0.2c"
 AUTHOR  = "Miroslav Stampar (http://unconciousmind.blogspot.com | @stamparm)"
 LICENSE = "Public domain (FREE)"
 
 PREFIXES = (" ", ") ", "' ", "') ", "\"")               # prefix values used for building testing blind payloads
-SUFFIXES = ("", "-- ", "#", "%00")                      # suffix values used for building testing blind payloads
+SUFFIXES = ("", "-- ", "#", "%00", "%16")               # suffix values used for building testing blind payloads
 TAMPER_SQL_CHAR_POOL = ('(', ')', '\'', '"')            # characters used for SQL tampering/poisoning of parameter values
 BOOLEAN_TESTS = ("AND %d=%d", "OR NOT (%d=%d)")         # boolean tests used for building testing blind payloads
 COOKIE, UA, REFERER = "Cookie", "User-Agent", "Referer" # optional HTTP header names
